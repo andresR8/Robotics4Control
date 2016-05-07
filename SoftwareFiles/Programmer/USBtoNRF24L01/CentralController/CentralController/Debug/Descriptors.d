a998c36b287acf6b13f14ead3077b6841751c7de
Andres Rengifo <anferesa239@gmail.com>
2016-05-07 11:19:39 -0500

    Debug Files

---
+++ SoftwareFiles/Programmer/USBtoNRF24L01/CentralController/CentralController/Debug/Descriptors.d
@@ -0,0 +1,350 @@
+Descriptors.d Descriptors.o: .././Descriptors.c .././Descriptors.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iousb162.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iousbxx2.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ .././Config/LUFAConfig.h .././LUFA/Drivers/USB/USB.h \
+ .././LUFA/Drivers/USB/../../Common/Common.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdbool.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\string.h \
+ .././LUFA/Drivers/USB/../../Common/Architectures.h \
+ .././LUFA/Drivers/USB/../../Common/BoardTypes.h \
+ .././LUFA/Drivers/USB/../../Common/ArchitectureSpecific.h \
+ .././LUFA/Drivers/USB/../../Common/CompilerSpecific.h \
+ .././LUFA/Drivers/USB/../../Common/Attributes.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\eeprom.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\boot.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include-fixed\limits.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ .././LUFA/Drivers/USB/../../Common/Endianness.h \
+ .././LUFA/Drivers/USB/Core/USBMode.h \
+ .././LUFA/Drivers/USB/Core/../../../Common/Common.h \
+ .././LUFA/Drivers/USB/Core/USBTask.h \
+ .././LUFA/Drivers/USB/Core/USBMode.h \
+ .././LUFA/Drivers/USB/Core/USBController.h \
+ .././LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../USBMode.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../Events.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../USBMode.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../USBTask.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/USBInterrupt_AVR8.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/../../../../Common/Common.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBMode.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/../Events.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBController.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../Device.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../StdDescriptors.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../Events.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../Endpoint.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/Endpoint_AVR8.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBTask.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBInterrupt.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/Device_AVR8.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/../StdDescriptors.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/../Endpoint.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../Endpoint.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../DeviceStandardReq.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../StdRequestType.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../USBTask.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../USBController.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../EndpointStream.h \
+ .././LUFA/Drivers/USB/Core/AVR8/../AVR8/EndpointStream_AVR8.h \
+ .././LUFA/Drivers/USB/Core/Events.h \
+ .././LUFA/Drivers/USB/Core/StdRequestType.h \
+ .././LUFA/Drivers/USB/Core/StdDescriptors.h \
+ .././LUFA/Drivers/USB/Core/DeviceStandardReq.h \
+ .././LUFA/Drivers/USB/Core/Events.h \
+ .././LUFA/Drivers/USB/Core/StdDescriptors.h \
+ .././LUFA/Drivers/USB/Core/ConfigDescriptors.h \
+ .././LUFA/Drivers/USB/Core/HostStandardReq.h \
+ .././LUFA/Drivers/USB/Core/USBController.h \
+ .././LUFA/Drivers/USB/Core/USBInterrupt.h \
+ .././LUFA/Drivers/USB/Core/Device.h \
+ .././LUFA/Drivers/USB/Core/Endpoint.h \
+ .././LUFA/Drivers/USB/Core/DeviceStandardReq.h \
+ .././LUFA/Drivers/USB/Core/EndpointStream.h \
+ .././LUFA/Drivers/USB/Class/AndroidAccessoryClass.h \
+ .././LUFA/Drivers/USB/Class/../Core/USBMode.h \
+ .././LUFA/Drivers/USB/Class/AudioClass.h \
+ .././LUFA/Drivers/USB/Class/Device/AudioClassDevice.h \
+ .././LUFA/Drivers/USB/Class/Device/../../USB.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/../../Core/StdDescriptors.h \
+ .././LUFA/Drivers/USB/Class/CDCClass.h \
+ .././LUFA/Drivers/USB/Class/Device/CDCClassDevice.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdarg.h \
+ .././LUFA/Drivers/USB/Class/HIDClass.h \
+ .././LUFA/Drivers/USB/Class/Device/HIDClassDevice.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/HIDParser.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/../../../../Common/Common.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/HIDReportData.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h \
+ .././LUFA/Drivers/USB/Class/MassStorageClass.h \
+ .././LUFA/Drivers/USB/Class/Device/MassStorageClassDevice.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/MassStorageClassCommon.h \
+ .././LUFA/Drivers/USB/Class/MIDIClass.h \
+ .././LUFA/Drivers/USB/Class/Device/MIDIClassDevice.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/MIDIClassCommon.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h \
+ .././LUFA/Drivers/USB/Class/PrinterClass.h \
+ .././LUFA/Drivers/USB/Class/Device/PrinterClassDevice.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/PrinterClassCommon.h \
+ .././LUFA/Drivers/USB/Class/RNDISClass.h \
+ .././LUFA/Drivers/USB/Class/Device/RNDISClassDevice.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/RNDISClassCommon.h \
+ .././LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h \
+ .././LUFA/Drivers/USB/Class/StillImageClass.h
+
+.././Descriptors.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iousb162.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\iousbxx2.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+.././Config/LUFAConfig.h:
+
+.././LUFA/Drivers/USB/USB.h:
+
+.././LUFA/Drivers/USB/../../Common/Common.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdbool.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\string.h:
+
+.././LUFA/Drivers/USB/../../Common/Architectures.h:
+
+.././LUFA/Drivers/USB/../../Common/BoardTypes.h:
+
+.././LUFA/Drivers/USB/../../Common/ArchitectureSpecific.h:
+
+.././LUFA/Drivers/USB/../../Common/CompilerSpecific.h:
+
+.././LUFA/Drivers/USB/../../Common/Attributes.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\eeprom.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\boot.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include-fixed\limits.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+.././LUFA/Drivers/USB/../../Common/Endianness.h:
+
+.././LUFA/Drivers/USB/Core/USBMode.h:
+
+.././LUFA/Drivers/USB/Core/../../../Common/Common.h:
+
+.././LUFA/Drivers/USB/Core/USBTask.h:
+
+.././LUFA/Drivers/USB/Core/USBMode.h:
+
+.././LUFA/Drivers/USB/Core/USBController.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../Events.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../USBTask.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/USBInterrupt_AVR8.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/../../../../Common/Common.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBMode.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/../Events.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBController.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../Device.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../StdDescriptors.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../Events.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../Endpoint.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/Endpoint_AVR8.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBTask.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBInterrupt.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/Device_AVR8.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/../StdDescriptors.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/../Endpoint.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../Endpoint.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../DeviceStandardReq.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../StdRequestType.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../USBTask.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../USBController.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../EndpointStream.h:
+
+.././LUFA/Drivers/USB/Core/AVR8/../AVR8/EndpointStream_AVR8.h:
+
+.././LUFA/Drivers/USB/Core/Events.h:
+
+.././LUFA/Drivers/USB/Core/StdRequestType.h:
+
+.././LUFA/Drivers/USB/Core/StdDescriptors.h:
+
+.././LUFA/Drivers/USB/Core/DeviceStandardReq.h:
+
+.././LUFA/Drivers/USB/Core/Events.h:
+
+.././LUFA/Drivers/USB/Core/StdDescriptors.h:
+
+.././LUFA/Drivers/USB/Core/ConfigDescriptors.h:
+
+.././LUFA/Drivers/USB/Core/HostStandardReq.h:
+
+.././LUFA/Drivers/USB/Core/USBController.h:
+
+.././LUFA/Drivers/USB/Core/USBInterrupt.h:
+
+.././LUFA/Drivers/USB/Core/Device.h:
+
+.././LUFA/Drivers/USB/Core/Endpoint.h:
+
+.././LUFA/Drivers/USB/Core/DeviceStandardReq.h:
+
+.././LUFA/Drivers/USB/Core/EndpointStream.h:
+
+.././LUFA/Drivers/USB/Class/AndroidAccessoryClass.h:
+
+.././LUFA/Drivers/USB/Class/../Core/USBMode.h:
+
+.././LUFA/Drivers/USB/Class/AudioClass.h:
+
+.././LUFA/Drivers/USB/Class/Device/AudioClassDevice.h:
+
+.././LUFA/Drivers/USB/Class/Device/../../USB.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/../../Core/StdDescriptors.h:
+
+.././LUFA/Drivers/USB/Class/CDCClass.h:
+
+.././LUFA/Drivers/USB/Class/Device/CDCClassDevice.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stdarg.h:
+
+.././LUFA/Drivers/USB/Class/HIDClass.h:
+
+.././LUFA/Drivers/USB/Class/Device/HIDClassDevice.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/HIDParser.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/../../../../Common/Common.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/HIDReportData.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h:
+
+.././LUFA/Drivers/USB/Class/MassStorageClass.h:
+
+.././LUFA/Drivers/USB/Class/Device/MassStorageClassDevice.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/MassStorageClassCommon.h:
+
+.././LUFA/Drivers/USB/Class/MIDIClass.h:
+
+.././LUFA/Drivers/USB/Class/Device/MIDIClassDevice.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/MIDIClassCommon.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h:
+
+.././LUFA/Drivers/USB/Class/PrinterClass.h:
+
+.././LUFA/Drivers/USB/Class/Device/PrinterClassDevice.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/PrinterClassCommon.h:
+
+.././LUFA/Drivers/USB/Class/RNDISClass.h:
+
+.././LUFA/Drivers/USB/Class/Device/RNDISClassDevice.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/RNDISClassCommon.h:
+
+.././LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h:
+
+.././LUFA/Drivers/USB/Class/StillImageClass.h:
